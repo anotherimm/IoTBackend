b8ae9722cfb55ea848f8fb8994f08aae93cffe38
imam160 <imambayhaqi114@gmail.com>
2025-02-09 11:36:13 +0700

    change code

--- api/index.js
+++ api/index.js
@@ -8,8 +8,8 @@ app.use(cors());
 app.use(express.json());
 
 // Koneksi ke Supabase
-const SUPABASE_URL = "https://yjvyjsemucsmtavoqxni.supabase.co";  // Ganti dengan URL Supabase kamu
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlqdnlqc2VtdWNzbXRhdm9xeG5pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkwNzM1NzIsImV4cCI6MjA1NDY0OTU3Mn0.IVAmki4qxAfFfoDbS15aBceheDZ_mikXf3vJM9zKFvI";  // Ganti dengan anon key dari Supabase
+const SUPABASE_URL = "https://qdrmhyptxrcigzexneoz.supabase.co";  // Ganti dengan URL Supabase kamu
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkcm1oeXB0eHJjaWd6ZXhuZW96Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkwNzQzNzIsImV4cCI6MjA1NDY1MDM3Mn0.igH_m_OayMlEr000Iih2Jq6n6wVLk04CpdMSI8EavSM";  // Ganti dengan anon key dari Supabase
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // Koneksi ke MQTT Broker
